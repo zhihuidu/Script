4b79a62561656c6420650ba9d61548f566e20587
Zhihui Du <zhihui@sherlock.cm.cluster>
2021-03-03 19:52:38 -0500

    add new script

---
+++ sherlock/mybenchmark/fit.py
@@ -0,0 +1,175 @@
+import pandas as pd
+import matplotlib as ml
+import numpy as np
+import sys
+import csv
+
+import numpy, scipy, scipy.optimize
+import matplotlib
+from mpl_toolkits.mplot3d import  Axes3D
+from matplotlib import cm # to colormap 3D surfaces from blue to red
+import matplotlib.pyplot as plt
+
+graphWidth = 800 # units are pixels
+graphHeight = 600 # units are pixels
+
+# 3D contour plot lines
+numberOfContourLines = 16
+
+#xData = numpy.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0])
+#yData = numpy.array([11.0, 12.1, 13.0, 14.1, 15.0, 16.1, 17.0, 18.1, 90.0])
+#zData = numpy.array([1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7, 8.0, 9.9])
+
+
+xData=numpy.array([393176,786396,1572823,3145686, 393176,786396,1572823,3145686])
+yData=numpy.array([2,2,2,2,4,4,4,4])
+zData=numpy.array([216.2043909,432.2506104,863.5975484,1756.460801,289.2458977,579.689231,1188.661483,2519.102987])
+# place the data in a single list
+data = [xData, yData, zData]
+
+
+def SurfacePlot(func, data, fittedParameters):
+    f = plt.figure(figsize=(graphWidth/100.0, graphHeight/100.0), dpi=100)
+
+    matplotlib.pyplot.grid(True)
+    axes = Axes3D(f)
+
+    # extract data from the single list
+    x_data = data[0]
+    y_data = data[1]
+    z_data = data[2]
+
+    xModel = numpy.linspace(min(x_data), max(x_data), 20)
+    yModel = numpy.linspace(min(y_data), max(y_data), 20)
+    X, Y = numpy.meshgrid(xModel, yModel)
+
+    Z = func(numpy.array([X, Y]), *fittedParameters)
+
+    axes.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm, linewidth=1, antialiased=True)
+
+    axes.scatter(x_data, y_data, z_data) # show data along with plotted surface
+
+    axes.set_title('Surface Plot (click-drag with mouse)') # add a title for surface plot
+    axes.set_xlabel('X Data') # X axis data label
+    axes.set_ylabel('Y Data') # Y axis data label
+    axes.set_zlabel('Z Data') # Z axis data label
+
+    plt.show()
+    plt.close('all') # clean up after using pyplot or else there can be memory and process problems
+
+
+def ContourPlot(func, data, fittedParameters):
+    f = plt.figure(figsize=(graphWidth/100.0, graphHeight/100.0), dpi=100)
+    axes = f.add_subplot(111)
+
+    # extract data from the single list
+    x_data = data[0]
+    y_data = data[1]
+    z_data = data[2]
+
+    xModel = numpy.linspace(min(x_data), max(x_data), 20)
+    yModel = numpy.linspace(min(y_data), max(y_data), 20)
+    X, Y = numpy.meshgrid(xModel, yModel)
+
+    Z = func(numpy.array([X, Y]), *fittedParameters)
+
+    axes.plot(x_data, y_data, 'o')
+
+    axes.set_title('Contour Plot') # add a title for contour plot
+    axes.set_xlabel('X Data') # X axis data label
+    axes.set_ylabel('Y Data') # Y axis data label
+
+    CS = matplotlib.pyplot.contour(X, Y, Z, numberOfContourLines, colors='k')
+    matplotlib.pyplot.clabel(CS, inline=1, fontsize=10) # labels for contours
+
+    plt.show()
+    plt.close('all') # clean up after using pyplot or else there can be memory and process problems
+
+
+def ScatterPlot(data):
+    f = plt.figure(figsize=(graphWidth/100.0, graphHeight/100.0), dpi=100)
+
+    matplotlib.pyplot.grid(True)
+    axes = Axes3D(f)
+
+    # extract data from the single list
+    x_data = data[0]
+    y_data = data[1]
+    z_data = data[2]
+
+    axes.scatter(x_data, y_data, z_data)
+
+    axes.set_title('Scatter Plot (click-drag with mouse)')
+    axes.set_xlabel('X Data')
+    axes.set_ylabel('Y Data')
+    axes.set_zlabel('Z Data')
+
+    plt.show()
+    plt.close('all') # clean up after using pyplot or else there can be memory and process problems
+
+
+def func(data, a, b, c):
+
+    # extract data from the single list
+    x1 = data[0]
+    x2 = data[1]
+
+    #return (a*x1/x2)+b*x1*x2+c
+    #return (a*x1)+b*x1*x2+c
+    #return (a*x1)+b*x2+c
+    return (a*x1/x2)+b*x1*x2+c
+
+
+if __name__ == "__main__":    
+    if len(sys.argv) <1:
+        print("No data file")
+        exit()
+
+    inputfile=sys.argv[1][:-4]
+    #df  = pd.read_csv(sys.argv[1],header=0,names=['0','1','2','3','4','5','6','7','8','9','10'])
+    df  = pd.read_csv(sys.argv[1],header=0,names=['0','1','2'])
+    
+    x1=df['0'] #number of edges
+    x2=df['1'] #number of locales
+    y=df['2']  #Building/BFS time
+    xData=x1
+    yData=x2
+    zData=y
+    data = [xData, yData, zData]
+
+    initialParameters = [1.0, 1.0, 1.0] # these are the same as scipy default values in this example
+
+    # here a non-linear surface fit is made with scipy's curve_fit()
+    fittedParameters, pcov = scipy.optimize.curve_fit(func, [xData, yData], zData, p0 = initialParameters)
+    modelPredictions = func(data, *fittedParameters) 
+    absError = modelPredictions - zData
+    SE = numpy.square(absError) # squared errors
+    MSE = numpy.mean(SE) # mean squared errors
+    RMSE = numpy.sqrt(MSE) # Root Mean Squared Error, RMSE
+    Rsquared = 1.0 - (numpy.var(absError) / numpy.var(zData))
+    print('RMSE:{:.2f}'.format(RMSE))
+    print('R-squared:{:.2f}'.format(Rsquared))
+    #print("T(E,L)= {:.2e} *E/L+ {:.2e} *L + {:.2e}".format(fittedParameters[0],fittedParameters[1],fittedParameters[2]))
+    #print("T(E,L)= {:.2e} *E+ {:.2e} *L + {:.2e}".format(fittedParameters[0],fittedParameters[1],fittedParameters[2]))
+    print("T(E,L)= {:.2e} *E/L+ {:.2e} *E*L + {:.2e}".format(fittedParameters[0],fittedParameters[1],fittedParameters[2]))
+    #print("T(Edges,Locales)=",fittedParameters[0],"*Edges/Locales+",fittedParameters[1],"*Locales+",fittedParameters[2])
+
+    for i in range(1,5):
+        print("T(E, {})= {:.2e} *E + {:.2e}".format(2**i, fittedParameters[0]/(2**i)+fittedParameters[1]*(2**i),fittedParameters[2]))
+        #print("T(E, {})= {:.2e} *E + {:.2e}".format(2**i, fittedParameters[0]/(2**i)+fittedParameters[1]*(2**i),fittedParameters[2]))
+        #print("T(E,",2**i,")=",(fittedParameters[0]/(2**i)),"*E+",fittedParameters[1]*(2**i)+fittedParameters[2])
+    exit(0)
+    ScatterPlot(data)
+    SurfacePlot(func, data, fittedParameters)
+    ContourPlot(func, data, fittedParameters)
+
+    print('fitted parameters', fittedParameters)
+
+    modelPredictions = func(data, *fittedParameters) 
+    absError = modelPredictions - zData
+    SE = numpy.square(absError) # squared errors
+    MSE = numpy.mean(SE) # mean squared errors
+    RMSE = numpy.sqrt(MSE) # Root Mean Squared Error, RMSE
+    Rsquared = 1.0 - (numpy.var(absError) / numpy.var(zData))
+    print('RMSE:', RMSE)
+    print('R-squared:', Rsquared)
