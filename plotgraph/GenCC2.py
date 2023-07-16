45327524eba4599d4806d9eeb8e2a55b95204714
Zhihui Du <zhihuidu@gmail.com>
2023-07-16 12:08:04 -0400

    add list attr script

--- plotgraph/GenCC2.py
+++ plotgraph/GenCC2.py
@@ -28,15 +28,18 @@ graph_name = data[0]
 
 # Extract iteration numbers
 iteration_numbers = data.iloc[:, 1:9]
-#print(iteration_numbers)
-bakdata=copy.deepcopy(iteration_numbers)
+print(iteration_numbers)
+exit(0)
+#bakdata=copy.deepcopy(iteration_numbers)
 # reorder the data
+'''
 iteration_numbers[1]=bakdata[3]
 iteration_numbers[2]=bakdata[1]
 iteration_numbers[3]=bakdata[7]
 iteration_numbers[4]=bakdata[5]
 iteration_numbers[5]=bakdata[6]
 iteration_numbers[6]=bakdata[4]
+'''
 #print(iteration_numbers)
 # remove data will not be shown in the paper
 #iteration_numbers.drop(iteration_numbers.columns[[6,7]], axis=1, inplace=True )
@@ -47,9 +50,9 @@ iteration_numbers[6]=bakdata[4]
 #plt.figure()
 #plt.tight_layout()
 plt.rcParams.update({'font.size': FontSize})
-ax=iteration_numbers.plot.bar(rot=0, figsize=(14, 10))
+ax=iteration_numbers.plot.bar(rot=0, figsize=(22, 10))
 
-ax.legend(["Contour", "FastSV", "ConnectIt","C-1","C-2","C-m", "C-11mm","C-Syn"])
+ax.legend(["FastSV", "ConnectIt","C-1m1m","C-1","C-2","C-m", "C-11mm","C-Syn"])
 plt.yscale('log',base=2) 
 #ax.set_yscale('symlog', basey=2)
 
@@ -80,8 +83,8 @@ print(pda)
 #plt.figure()
 #plt.tight_layout()
 plt.rcParams.update({'font.size': FontSize})
-ax=pda.plot.bar(rot=0, figsize=(14, 10))
-ax.legend(["Contour", "FastSV", "ConnectIt","C-1","C-2","C-m", "C-11mm","C-Syn"])
+ax=pda.plot.bar(rot=0, figsize=(22, 10))
+ax.legend(["FastSV", "ConnectIt","C-1m1m","C-1","C-2","C-m", "C-11mm","C-Syn"])
 #ax.set_yscale('symlog', basey=10)
 plt.yscale('log',base=2) 
 plt.ylabel("Execution Time(s)")
@@ -107,17 +110,18 @@ execution_values.drop(execution_values.columns[[6,7]], axis=1, inplace=True )
 print(execution_values)
 '''
 inverse=1/pda
-speedup=inverse.multiply(pda.iloc[:,2], axis=0)
+speedup=inverse.multiply(pda.iloc[:,0], axis=0)
 print(speedup)
 #plt.tight_layout()
 plt.rcParams.update({'font.size': FontSize})
-ax=speedup.plot.bar(rot=0, figsize=(14, 10))
-ax.legend(["Contour", "FastSV", "ConnectIt","C-1","C-2","C-m", "C-11mm","C-Syn"],ncol=4,loc="lower center")
+ax=speedup.plot.bar(rot=0, figsize=(22, 10))
+#ax.legend(["FastSV", "ConnectIt","C-1m1m","C-1","C-2","C-m", "C-11mm","C-Syn"],ncol=4,loc="lower center")
+ax.legend(["FastSV", "ConnectIt","C-1m1m","C-1","C-2","C-m", "C-11mm","C-Syn"],ncol=2)
 #ax.set_yscale('symlog', basey=2)
-plt.yscale('log',base=2) 
+#plt.yscale('log',base=2) 
 plt.ylabel("Speedup")
 plt.xlabel("Graph ID")
-plt.title("Comparison of Speedups")
+plt.title("Speedup Comparison (Baseline:FastSV)")
 #plt.legend(ncol=4)
 plt.subplots_adjust(bottom=0.19)
 plt.savefig(filename+"-Speedup.png")
@@ -127,6 +131,25 @@ plt.close()
 
 all=pd.concat([data, speedup],axis=1)
 all.to_csv(filename+"-all.csv")
-all=pd.concat([data, speedup],axis=1)
-all.to_excel(filename+"-all.xlsx")
+#all=pd.concat([data, speedup],axis=1)
+#all.to_excel(filename+"-all.xlsx")
 
+inverse2=1/pda
+speedup2=inverse.multiply(pda.iloc[:,1], axis=0)
+print(speedup2)
+speedup2.drop(speedup2.columns[[0,7]], axis=1, inplace=True )
+#plt.tight_layout()
+plt.rcParams.update({'font.size': FontSize})
+ax=speedup2.plot.bar(rot=0, figsize=(22, 10))
+#ax.legend(["FastSV", "ConnectIt","C-1m1m","C-1","C-2","C-m", "C-11mm","C-Syn"],ncol=4,loc="lower center")
+ax.legend(["ConnectIt","C-1m1m","C-1","C-2","C-m", "C-11mm"],ncol=2)
+#ax.set_yscale('symlog', basey=2)
+#plt.yscale('log',base=2) 
+plt.ylabel("Speedup")
+plt.xlabel("Graph ID")
+plt.title("Speedup Comparison (Baseline:ConnectIt)")
+#plt.legend(ncol=4)
+plt.subplots_adjust(bottom=0.19)
+plt.savefig(filename+"-Speedup2.png")
+plt.show()
+plt.close()
---
+++ plotgraph/ListFileAttr.py
@@ -0,0 +1,164 @@
+#!/usr/bin/env python3                                                         
+
+import time, argparse
+import numpy as np
+import arkouda as ak
+import random
+import string
+import arachne_development.graph as njit
+import sys
+import subprocess
+
+import os
+import csv
+import pathlib
+def myfunc():
+
+    HomeDir="/rhome/zhihui/"
+
+    EdgeList=[ [28980,5242,2,0,HomeDir+"Adata/SNAP/ca-GrQc.txt.gr"],\
+            [51971,9877,2,0,HomeDir+"Adata/SNAP/ca-HepTh.txt.gr"],\
+            [88234,4039,2,0,HomeDir+"Adata/SNAP/facebook_combined.txt"],\
+            [103689,8277,2,0,HomeDir+"Adata/SNAP/wiki"],\
+            [106762,26475,3,0,HomeDir+"Adata/SNAP/as-caida20071105.txt.gr"],\
+            [186936,23133,2,0,HomeDir+"Adata/SNAP/ca-CondMat.txt.gr"],\
+            [237010,12008,2,0,HomeDir+"Adata/SNAP/ca-HepPh.txt.gr"],\
+            [367662,36692,2,0,HomeDir+"Adata/SNAP/email-Enron.gr"],\
+            [396160,18772,2,0,HomeDir+"Adata/SNAP/ca-AstroPh.txt.gr"],\
+            [428156,58228,2,0,HomeDir+"Adata/SNAP/loc-brightkite_edges.txt"],\
+            [508837,75879,2,0,HomeDir+"Adata/SNAP/soc-Epinions1.txt.gr"],\
+            [1049866,317080,2,0,HomeDir+"Adata/SNAP/com-dblp.ungraph.txt.gr"],\
+            [2987624,1134890,2,0,HomeDir+"Adata/SNAP/com-youtube.ungraph.txt.gr"],\
+            [2443408,403394,2,0,HomeDir+"Adata/SNAP/amazon0601.txt.pr"],\
+            [68993773,4847571,2,0,HomeDir+"Adata/SNAP/soc-LiveJournal1.txt"],\
+            [14855842,456626,2,0,HomeDir+"Adata/higgs-social_network.edgelist"],\
+            [117185083,3072441,2,0,HomeDir+"Adata/com-orkut.ungraph.txt.gr"],\
+            ]
+
+    MtxDelaunay=[ [3056,1024,2,0,HomeDir+"Adata/Delaunay/delaunay_n10/delaunay_n10.mtx"],\
+            [6127,2048,2,0,HomeDir+"Adata/Delaunay/delaunay_n11/delaunay_n11.mtx"] ,\
+            [12264, 4096,2,0,HomeDir+"Adata/Delaunay/delaunay_n12/delaunay_n12.mtx"] ,\
+            [24547,8192,2,0,HomeDir+"Adata/Delaunay/delaunay_n13/delaunay_n13.mtx"] ,\
+            [49122,16384,2,0,HomeDir+"Adata/Delaunay/delaunay_n14/delaunay_n14.mtx"] ,\
+            [98274,32768,2,0,HomeDir+"Adata/Delaunay/delaunay_n15/delaunay_n15.mtx"] ,\
+            [196575,65536,2,0,HomeDir+"Adata/Delaunay/delaunay_n16/delaunay_n16.mtx"],\
+            [393176,131072,2,0,HomeDir+"Adata/Delaunay/delaunay_n17/delaunay_n17.mtx"],\
+            [786396,262144,2,0,HomeDir+"Adata/Delaunay/delaunay_n18/delaunay_n18.mtx"],\
+            [1572823,524288,2,0,HomeDir+"Adata/Delaunay/delaunay_n19/delaunay_n19.mtx"],\
+            [3145686,1048576,2,0,HomeDir+"Adata/Delaunay/delaunay_n20/delaunay_n20.mtx"],\
+            [6291408,2097152,2,0,HomeDir+"Adata/Delaunay/delaunay_n21/delaunay_n21.mtx"],\
+            [12582869,4194304,2,0,HomeDir+"Adata/Delaunay/delaunay_n22/delaunay_n22.mtx"],\
+            [25165784,8388608,2,0,HomeDir+"Adata/Delaunay/delaunay_n23/delaunay_n23.mtx"],\
+            [50331601,16777216,2,0,HomeDir+"Adata/Delaunay/delaunay_n24/delaunay_n24.mtx"],\
+            ]
+    MtxOther=[[28854312,23947347,2,0,HomeDir+"Adata/road_usa/road_usa.mtx"],\
+            ]
+    BigMtxFile=[ 
+            [180292586,170728175,2,0,HomeDir+"Adata/SNAP/kmer_A2a.mtx"],\
+            [232705452,214005017,2,0,HomeDir+"Adata/SNAP/kmer_V1r.mtx"],\
+            [298113762,18520486,2,0,HomeDir+"Adata/SNAP/uk-2002.mtx"],\
+#            [936364282,39459925,2,0,HomeDir+"Adata/SNAP/uk-2005.mtx"],\
+              ]
+    MtxRGG=[ \
+            [14487995,2097152,2,0,HomeDir+"Adata/rgg_n_2/rgg_n_2_21_s0/rgg_n_2_21_s0.mtx"],\
+            [30359198,4194304,2,0,HomeDir+"Adata/rgg_n_2/rgg_n_2_22_s0/rgg_n_2_22_s0.mtx"],\
+            [63501393,8388608,2,0,HomeDir+"Adata/rgg_n_2/rgg_n_2_23_s0/rgg_n_2_23_s0.mtx"],\
+            [132557200,16777216,2,0,HomeDir+"Adata/rgg_n_2/rgg_n_2_24_s0/rgg_n_2_24_s0.mtx"],\
+              ]
+    MtxKron=[ \
+            [2456398,65536,3,0,HomeDir+"Adata/kron_g500-logn/kron_g500-logn16/kron_g500-logn16.mtx"],\
+            [5114375,131072,3,0,HomeDir+"Adata/kron_g500-logn/kron_g500-logn17/kron_g500-logn17.mtx"],\
+            [10583222,262144,3,0,HomeDir+"Adata/kron_g500-logn/kron_g500-logn18/kron_g500-logn18.mtx"],\
+            [21781478,524288,3,0,HomeDir+"Adata/kron_g500-logn/kron_g500-logn19/kron_g500-logn19.mtx"],\
+            [44620272,1048576,3,0,HomeDir+"Adata/kron_g500-logn/kron_g500-logn20/kron_g500-logn20.mtx"],\
+              ]
+    ProSNAP=[ [14484,5242,2,0,HomeDir+"Adata/SNAP/ca-GrQc.txt.pr"],\
+            [25973,9877,2,0,HomeDir+"Adata/SNAP/ca-HepTh.txt.pr"],\
+            [53381,26475,2,0,HomeDir+"Adata/SNAP/as-caida20071105.txt.pr"],\
+            [88234,4039,2,0,HomeDir+"Adata/SNAP/facebook_combined.txt.pr"],\
+            [93439,23133,2,0,HomeDir+"Adata/SNAP/ca-CondMat.txt.pr"],\
+            [118489,12008,2,0,HomeDir+"Adata/SNAP/ca-HepPh.txt.pr"],\
+            [198050,18772,2,0,HomeDir+"Adata/SNAP/ca-AstroPh.txt.pr"],\
+            [1049866,317080,2,0,HomeDir+'Adata/SNAP/com-dblp.ungraph.txt.gr.pr'],\
+            [183831,36692,2,0,HomeDir+"Adata/SNAP/email-Enron.gr.pr"],\
+            [214078,58228,2,0,HomeDir+"Adata/SNAP/loc-brightkite_edges.txt.pr"],\
+            [405740,75879,2,0,HomeDir+"Adata/SNAP/soc-Epinions1.txt.pr"],\
+            [2443408,403394,2,0,HomeDir+"Adata/SNAP/amazon0601.txt.pr"],\
+            [2987624,1134890,2,0,HomeDir+"Adata/SNAP/com-youtube.ungraph.txt.pr"],\
+            [25004123,4847571,2,0,HomeDir+'Adata/SNAP/soc-LiveJournal1.txt.pr']
+            ]
+    ProDelaunay=[ [3056,1024,2,0,HomeDir+"Adata/Delaunay/delaunay_n10/delaunay_n10.mtx.pr"],\
+            [6127,2048,2,0,HomeDir+"Adata/Delaunay/delaunay_n11/delaunay_n11.mtx.pr"] ,\
+            [12264, 4096,2,0,HomeDir+"Adata/Delaunay/delaunay_n12/delaunay_n12.mtx.pr"] ,\
+            [24547,8192,2,0,HomeDir+"Adata/Delaunay/delaunay_n13/delaunay_n13.mtx.pr"] ,\
+            [49122,16384,2,0,HomeDir+"Adata/Delaunay/delaunay_n14/delaunay_n14.mtx.pr"] ,\
+            [98274,32768,2,0,HomeDir+"Adata/Delaunay/delaunay_n15/delaunay_n15.mtx.pr"] ,\
+            [196575,65536,2,0,HomeDir+"Adata/Delaunay/delaunay_n16/delaunay_n16.mtx.pr"],\
+            [393176,131072,2,0,HomeDir+"Adata/Delaunay/delaunay_n17/delaunay_n17.mtx.pr"],\
+            [786396,262144,2,0,HomeDir+"Adata/Delaunay/delaunay_n18/delaunay_n18.mtx.pr"],\
+            [1572823,524288,2,0,HomeDir+"Adata/Delaunay/delaunay_n19/delaunay_n19.mtx.pr"],\
+            [3145686,1048576,2,0,HomeDir+"Adata/Delaunay/delaunay_n20/delaunay_n20.mtx.pr"],\
+            [6291408,2097152,2,0,HomeDir+"Adata/Delaunay/delaunay_n21/delaunay_n21.mtx.pr"],\
+            [12582869,4194304,2,0,HomeDir+"Adata/Delaunay/delaunay_n22/delaunay_n22.mtx.pr"],\
+            [25165784,8388608,2,0,HomeDir+"Adata/Delaunay/delaunay_n23/delaunay_n23.mtx.pr"],\
+            [50331601,16777216,2,0,HomeDir+"Adata/Delaunay/delaunay_n24/delaunay_n24.mtx.pr"],\
+             ]
+    ProPBig=[ [14855842,456626,2,0,HomeDir+"Adata/higgs-social_network.edgelist"],\
+            [117185083,3072441,2,0,HomeDir+"Adata/com-orkut.ungraph.txt"],\
+            [180292586,170728175,2,0,HomeDir+"Adata/SNAP/kmer_A2a.mtx.pr"],\
+            [232705452,214005017,2,0,HomeDir+"Adata/SNAP/kmer_V1r.mtx.pr"],\
+            [261787258,18484117,2,0,HomeDir+"Adata/SNAP/uk-2002.mtx.pr"],\
+            [783027125,39454746,2,0,HomeDir+"Adata/SNAP/uk-2005.mtx.pr"],\
+             ]
+
+    TestMtx=[ [28854312,23947347,2,0,HomeDir+"Adata/road_usa/road_usa.mtx"],\
+              ]
+    ProRGG=[ \
+            [14487995,2097148,2,0,HomeDir+"Adata/rgg_n_2/rgg_n_2_21_s0/rgg_n_2_21_s0.mtx.pr"],\
+            [30359198,4194301,2,0,HomeDir+"Adata/rgg_n_2/rgg_n_2_22_s0/rgg_n_2_22_s0.mtx.pr"],\
+            [63501393,8388607,2,0,HomeDir+"Adata/rgg_n_2/rgg_n_2_23_s0/rgg_n_2_23_s0.mtx.pr"],\
+            [132557200,16777215,2,0,HomeDir+"Adata/rgg_n_2/rgg_n_2_24_s0/rgg_n_2_24_s0.mtx.pr"],\
+              ]
+    ProKron=[ \
+            [2456071,55321,3,0,HomeDir+"Adata/kron_g500-logn/kron_g500-logn16/kron_g500-logn16.mtx.pr"],\
+            [5113985,107909,3,0,HomeDir+"Adata/kron_g500-logn/kron_g500-logn17/kron_g500-logn17.mtx.pr"],\
+            [10582686,210155,3,0,HomeDir+"Adata/kron_g500-logn/kron_g500-logn18/kron_g500-logn18.mtx.pr"],\
+            [21780787,409175,3,0,HomeDir+"Adata/kron_g500-logn/kron_g500-logn19/kron_g500-logn19.mtx.pr"],\
+            [44619402,795241,3,0,HomeDir+"Adata/kron_g500-logn/kron_g500-logn20/kron_g500-logn20.mtx.pr"],\
+              ]
+
+    output_file = "myoutput.csv"
+
+    with open(output_file, "w", newline="") as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(["Graph Name","Graph ID","Number of Edges", "Number of Vertices"])
+        ID=0
+        for mylist in [EdgeList,MtxOther, BigMtxFile,MtxDelaunay]:
+            for item in mylist:
+                filename = pathlib.Path(item[4])
+                extensions = filename.suffixes
+                print("The file extensions are:", extensions)
+                for extension in extensions:
+                      filename = filename.stem
+                      filename = pathlib.Path(filename)
+                      print("The output filename is:", filename)
+                shortname = filename
+                print("The output filename:", shortname)
+                csv_writer.writerow([shortname,ID,item[0], item[1]])
+                ID=ID+1
+    with open('myoutput.csv', 'r') as infile:
+            reader = csv.reader(infile)
+            data = list(reader)
+            transposed_data = list(map(list, zip(*data)))
+            with open('output_transposed.csv', 'w', newline='') as outfile:
+                 writer = csv.writer(outfile)
+                 for row in transposed_data:
+                     writer.writerow(row)
+
+    return
+
+
+if __name__ == "__main__":
+    import sys
+    
+    myfunc()
