286072ee3c7571cb0cf6447b45deaa0edd1843b0
Zhihui Du <zhihuidu@gmail.com>
2023-11-11 19:34:19 -0500

    add CleanAll.sh or connected component output

---
+++ bin/StartServer.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#SBATCH --sockets-per-node=2
+#SBATCH --cores-per-socket=10
+#SBATCH --threads-per-core=1
+hostname=`hostname`
+
+PortNum=$2
+d=`date +%F-%H-%M-%S`
+NodeList=`scontrol show hostname| paste -d- -s`
+output="$d-$hostname-Server-$1-Locales-$PortNum-$NodeList"
+# Only argument is number of locales/nodes
+if [ "$#" -le 1 ];
+then
+    echo "usage: ./StartServer   <#locales> <PortNum> "
+    exit
+fi
+numNodes=$1
+
+# Get a list of the nodes allocated to this job
+temp=`scontrol show hostname | paste -d, -s`
+IFS=',' read -ra NODES <<< "$temp"
+echo "+ Running arkouda_server with ${numNodes} locales on the following nodes:" 
+for node in "${NODES[@]}"
+do
+    echo ${node} 
+    ~/bin/Knode ${node}
+done
+
+# Finally, start arkouda with the specified number of locales
+#./arkouda_server -nl ${numNodes} &> server_out.txt
+
+
+echo "./arkouda_server -nl $1 --ServerConfig.ServerPort=$PortNum &> $output"
+
+mkdir -p AKOutPut
+#echo "./arkouda_server -nl $1 2>&1 |tee -a $output"
+#./arkouda_server -nl $1 2>&1 |tee -a $output &
+./arkouda_server -nl $1 --ServerConfig.ServerPort=$PortNum 2>&1|tee -a AKOutPut/$output
